29785633b1dd17777031d01940c3661b2bed6b72
Kaike <kaikedias98@gmail.com>
2024-06-05 10:25:45 -0300

    initial commit

---
+++ src/test/java/com/example/nutriapp/NutriAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nutriapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutriAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
